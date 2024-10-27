fd450a25f3b91dcb3cbcf13d9ca1fbca58d22f89
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-10-27 11:24:05 +0400

    i made some changes

--- TestGit/Controllers/WeatherForecastController.cs
+++ TestGit/Controllers/WeatherForecastController.cs
@@ -18,6 +18,18 @@ namespace TestGit.Controllers
             _logger = logger;
         }
 
+        [HttpGet("WeatherForecast")]
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
